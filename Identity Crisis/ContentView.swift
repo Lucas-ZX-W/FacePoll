7dc27fbc70fe1dfc2c0ecacfd66cf278c60d618c
Lucas Wang <lucaswzx@icloud.com>
2020-10-17 16:51:19 -0700

    Initial Commit

---
+++ Identity Crisis/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Identity Crisis
+//
+//  Created by Lucas Wang on 2020-10-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
